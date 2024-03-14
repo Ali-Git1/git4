68236b5be6d12e47773e28f65f4308726e703d34
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-15 00:37:52 +0400

    commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Code:
+            //    try
+            //    {
+
+            //        Console.WriteLine("N ededini daxil edin");
+            //        int n = Convert.ToInt32(Console.ReadLine());
+
+            //        for (int j = 1; j <= n; j++)
+            //        {
+            //            for (int i = 1; i <= n; i++)
+            //            {
+            //                Console.WriteLine("{0}x{1}={2}", i, j, (i * j));
+            //            }
+            //        }
+            //        Console.ReadLine();
+            //    }
+            //    catch (Exception)
+            //    {
+
+            //        Console.WriteLine("Emeliyyat dogru deyil");
+            //        goto Code;
+
+            //    }
+
+
+            
+        }
+    }
+}
